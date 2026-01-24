4dd4c9b4447e5defd7a95a741d76662037331cdb
Exploring-glitch <deysasha262@gmail.com>
2026-01-24 20:13:37 +0530

    adding the necessities

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,27 @@ dotenv.config({path: "./.env"});
 import cors from "cors"; 
 import path from "path";
 
+
 app.use(cors({
     origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
+
+//middleware
 app.use(express.json)
+
+
+//Routes
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/tasks", taskRoutes)
+app.use("/api/reports", reportRoutes)
+
+
+//server start
+const PORT = process.env.PORT || 3000;
+    app.listen( PORT, () => {
+    console.log("Server is running on port");
+})
