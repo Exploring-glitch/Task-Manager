e02fae85c5784c97184806681c59810ffd4add3e
Exploring-glitch <deysasha262@gmail.com>
2026-01-25 12:48:20 +0530

    defined the user schema

---
+++ backend/src/models/userSchema.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
+
+        profileImageUrl: {
+            type: String,
+            default: null
+        },
+
+        role: {
+            type: String,
+            enum: ["admin", "member"],
+            default: "member"
+        },
+
+    }, 
+    { timestamps: true }
+)
+
+
+
+const userModel = mongoose.model("user", userSchema);
+export default userModel;
