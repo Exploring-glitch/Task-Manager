69468c581c2ab0c66165793726c26d077ee21b68
Exploring-glitch <deysasha262@gmail.com>
2026-02-01 13:49:03 +0530

    coded the Tasks export function

--- backend/src/controller/reportController.js
+++ backend/src/controller/reportController.js
@@ -1,8 +1,49 @@
+import User from "../models/userSchema.js";
+import Task from "../models/taskSchema.js";
+import excelJs from "exceljs";
 
-
-export const exportTasksReport = async() => {
+export const exportTasksReport = async(req,res) => {
     try{
+        const tasks = await Task.find().populate("assignedTo", "name email");
 
+        const workBook = new excelJs.Workbook();
+        const workSheet = workBook.addWorksheet("Task Report");
+        workSheet.columns = [
+            {header : "Task Id", key : "._id", width : 25},
+            {header : "Title", key : "title", width : 30},
+            {header : "Description", key : "description", width : 50},
+            {header : "Priority", key : "priority", width : 15},
+            {header : "Status", key : "status", width : 20},
+            {header : "Due Date", key : "dueDate", width : 20},
+            {header : "Assigned To", key : "assignedTo", width : 30}
+
+        ]
+        tasks.forEach((task) => {
+            const assignedTo = task.assignedTo
+                .map((user) => `${user.name} (${user.email})`)
+                .join(", ");
+            workSheet.addRow({
+                _id : task._id,
+                title : task.title,
+                description : task.description,
+                priority : task.priority,
+                status: task.status,
+                dueDate : task.dueDate.toISOString().split("T")[0],
+                assignedTo : assignedTo || "Unassigned",
+            });
+        });
+        res.setHeader(
+            "Content-Type",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        );
+        res.setHeader(
+            "Content-Disposition",
+            'attachment; filename="tasks_report.xlsx"'
+        );
+
+        return workBook.xlsx.write(res).then(() =>{
+            res.end();
+        });
     }
     catch(error){
         resizeBy.status(500).json({ "message": "Error exporting tasks", "error": error.message })
