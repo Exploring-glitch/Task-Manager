24e263b3aa40af9a267d5bddbffd0de512162d30
Exploring-glitch <deysasha262@gmail.com>
2026-02-02 23:08:24 +0530

    defining the login page in frontend

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import AuthLayout from '../../components/layout/AuthLayout.jsx'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <AuthLayout>Login</AuthLayout>
   )
 }
 
