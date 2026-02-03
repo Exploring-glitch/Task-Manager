28314dda2a8b624f58956795d8d733cce61a15b2
Exploring-glitch <deysasha262@gmail.com>
2026-02-03 19:18:02 +0530

    changed the structure of the code

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
         setError("Please enter a valid email address")
         return;
       }
-      if(!password){
+      if (!password) {
         setError("Please enter the password")
       }
 
@@ -31,52 +31,50 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <div className='mt-52 p-2 w-2xl'>
-        <h2 className="text-3xl font-bold text-[#1E63E6]">Welcome Back</h2>
-        <h6 className='text-gray-600 mb-8'>Please enter your details to login</h6>
+    <div className='mt-52 p-2 w-2xl'>
+      <h2 className="text-3xl font-bold text-[#1E63E6]">Welcome Back</h2>
+      <h6 className='text-gray-600 mb-8'>Please enter your details to login</h6>
 
-        {error && (
-          <div className="mb-4 p-2 sm:p-3 bg-[#FFF1F2] text-[#E11D48] border border-[#FECDD3] rounded-md">
-            {error}
-          </div>
-        )}
-
-        <div className='mb-4'>
-          <label className="text-sm font-semibold" htmlFor="email"> Enter your email adress </label>
-          <input
-            value={email}
-            onInput={(c) => { setEmail(c.target.value) }}
-            type="email" placeholder='Example: alex@example.com'
-            className='mt-2 placeholder:text-sm text-sm bg-[#F5F8FF] border-2 border-[#D6E0FF] rounded w-full p-2 focus:outline-none focus:ring-2 focus:ring-[#2979FF]'
-          />
+      {error && (
+        <div className="mb-4 p-2 sm:p-3 bg-[#FFF1F2] text-[#E11D48] border border-[#FECDD3] rounded-md">
+          {error}
         </div>
+      )}
 
-        <div className='mb-4'>
-          <label className="text-sm font-semibold" htmlFor="password"> Enter your password here </label>
-          <input
-            value={password}
-            onInput={(c) => { setPassword(c.target.value) }}
-            type="password" placeholder='Minimum 8 characters needed'
-            className='mt-2 placeholder:text-sm text-sm bg-[#F5F8FF] border-2 border-[#D6E0FF] rounded w-full p-2 focus:outline-none focus:ring-2 focus:ring-[#2979FF]'
-          />
-        </div>
+      <div className='mb-4'>
+        <label className="text-sm font-semibold" htmlFor="email"> Enter your email adress </label>
+        <input
+          value={email}
+          onInput={(c) => { setEmail(c.target.value) }}
+          type="email" placeholder='Example: alex@example.com'
+          className='mt-2 placeholder:text-sm text-sm bg-[#F5F8FF] border-2 border-[#D6E0FF] rounded w-full p-2 focus:outline-none focus:ring-2 focus:ring-[#2979FF]'
+        />
+      </div>
 
-        <div className="mb-3">
-          <button
-            onClick={handleSubmit}
-            disabled={loading}
-            className="bg-[#1D4ED8] hover:bg-[#1E63E6] text-white cursor-pointer transition-colors duration-200 font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-          >
-            {loading ? 'ᯓ ✈︎' : 'LOGIN'}
-          </button>
-        </div>
+      <div className='mb-4'>
+        <label className="text-sm font-semibold" htmlFor="password"> Enter your password here </label>
+        <input
+          value={password}
+          onInput={(c) => { setPassword(c.target.value) }}
+          type="password" placeholder='Minimum 8 characters needed'
+          className='mt-2 placeholder:text-sm text-sm bg-[#F5F8FF] border-2 border-[#D6E0FF] rounded w-full p-2 focus:outline-none focus:ring-2 focus:ring-[#2979FF]'
+        />
+      </div>
 
-        <div className="text-center">
-          <p className="cursor-pointer text-sm text-gray-600">
-            Don't have an account? <span className="text-[#1E63E6] hover:text-[#1D4ED8] font-semibold"><u>SignUp</u></span>
-          </p>
-        </div>
+      <div className="mb-3">
+        <button
+          onClick={handleSubmit}
+          disabled={loading}
+          className="bg-[#1D4ED8] hover:bg-[#1E63E6] text-white cursor-pointer transition-colors duration-200 font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+        >
+          {loading ? 'ᯓ ✈︎' : 'LOGIN'}
+        </button>
+      </div>
+
+      <div className="text-center">
+        <p className="cursor-pointer text-sm text-gray-600">
+          Don't have an account? <span className="text-[#1E63E6] hover:text-[#1D4ED8] font-semibold"><u>SignUp</u></span>
+        </p>
       </div>
     </div>
   )
