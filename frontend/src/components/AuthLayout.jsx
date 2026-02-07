90a25866c86fca4a2be52e6152c28a6209658328
Exploring-glitch <deysasha262@gmail.com>
2026-02-07 21:08:58 +0530

    changes

--- frontend/src/components/AuthLayout.jsx
+++ frontend/src/components/AuthLayout.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className='w-screen h-full overflow-hidden'>
-      <div className=''>
+    <div className='w-screen h-full overflow-hidden bg-linear-to-br from-[#F8FAFC] via-[#EEF4FF] to-[#E8F0FF]'>
+      <div className='w-1/2'>
+        
         {children}
+      
       </div>
 
       <div>
@@ -12,18 +14,17 @@ const AuthLayout = ({ children }) => {
           src="https://i.pinimg.com/736x/82/16/34/82163432e70009ce3c4707ec7e36f4e1.jpg"
           alt=""
           className="
-          hidden sm:block
+          hidden md:block
           mix-blend-multiply 
           
-          sm:object-cover
-          sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 
-          sm:h-[50%] sm:w-[50%]
-          
-          md:h-[60%] md:w-[60%]
+          md:fixed
+          md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 
+          md:h-[50vh] md:w-auto
+        
 
-          lg:fixed lg:top-90 lg:right-
-          lg:h-screen
-          lg:w-1/2 xl:w-[43%]"
+          lg:fixed lg:top-0 lg:right-0 lg:left-auto
+          lg:translate-x-0 lg:translate-y-0
+          lg:h-screen w-[50%]"
         />
       </div>
     </div>
