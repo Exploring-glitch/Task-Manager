e75d4e8380f49662425aff0cd30bc2ef2a5a8808
Exploring-glitch <deysasha262@gmail.com>
2026-02-13 23:32:50 +0530

    coded and tested the profile photo upload and delete functionality

--- frontend/src/components/ProfilePhotoSelector.jsx
+++ frontend/src/components/ProfilePhotoSelector.jsx
@@ -7,13 +7,13 @@ const ProfilePhotoSelector = ({ image, setImage }) => {
 
     const handleImageChange = (e) => {
         const file = e.target.files[0];
-        if(file){
-           //update the image this state
-           setImage(file);
+        if (file) {
+            //update the image this state
+            setImage(file);
 
-           //generate preview url from the file
-           const preview = URL.createObjectURL(file);
-           setPreviewUrl(preview);
+            //generate preview url from the file
+            const preview = URL.createObjectURL(file);
+            setPreviewUrl(preview);
         }
     };
 
@@ -27,12 +27,45 @@ const ProfilePhotoSelector = ({ image, setImage }) => {
     };
 
     return (
-        <div className=''>
-            <input 
+        <div className='flex justify-center mb-6'>
+            <input
                 type="file"
                 accept='image/*'
                 ref={inputRef}
-                onChange={handleImageChange} />
+                onChange={handleImageChange}
+                className='hidden'
+            />
+
+            {!image ? (
+                <div className='bg-blue-100 w-20 h-20 flex justify-center items-center rounded-full
+                    relative cursor-pointer'
+                >
+                    <LuUser className='text-4xl'></LuUser>
+
+                    <button
+                        type='button'
+                        className='bg-blue-800 text-white w-8 h-8 flex justify-center items-center rounded-full
+                        absolute left-14 bottom-11 cursor-pointer'
+                        onClick={onChooseFile}
+                    >   <LuUpload />
+                    </button>
+                </div>
+            ) : (
+                <div className='relative'>
+                    <img
+                        src={previewUrl}
+                        alt='profile photo'
+                        className='w-20 h-20 rounded-full object-cover'
+                    />
+                    <button
+                        type='button'
+                        className='bg-red-800 text-white w-8 h-8 flex justify-center items-center rounded-full
+                        absolute left-14 bottom-11 cursor-pointer'
+                        onClick={handleRemoveImage}
+                    >   <LuTrash />
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
