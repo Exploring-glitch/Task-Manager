ff69733b99e669eca1491f02a784f0fecf9d807c
Exploring-glitch <deysasha262@gmail.com>
2026-02-14 21:48:01 +0530

    coding the context

---
+++ frontend/src/context/userContext.jsx
@@ -0,0 +1,8 @@
+import React,{ createContext, useState, useEffect, Children } from "react";
+import { axiosInstance } from "../util/axiosInstance.js";
+
+export const UserContext = createContext();
+
+const UserProvider = ({Children}) => {
+
+}
