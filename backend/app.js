466913c6f173caef461ed34c96871cf2abdddef1
Exploring-glitch <deysasha262@gmail.com>
2026-02-16 23:02:42 +0530

    feat: enable serving profile images from uploads directory

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,14 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config({path: "./.env"});
 import cors from "cors"; 
+import path from "path";
 import { connectDb } from "./src/config/mongoConfig.js";
 import authRouter from "./src/routes/authRoutes.js";
 import userRouter from "./src/routes/userRoutes.js";
 import taskRouter from "./src/routes/taskRoutes.js";
 import reportRouter from "./src/routes/reportRoutes.js"
+
+import { fileURLToPath } from "url";
 
 
 app.use(cors({
@@ -21,11 +24,19 @@ app.use(cors({
 app.use(express.json());
 
 
+//Serve uploads folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+
 //Routes
 app.use("/api/auth", authRouter)
 app.use("/api/users", userRouter)
 app.use("/api/tasks", taskRouter)
 app.use("/api/reports", reportRouter)
+
+
 
 
 //server start
