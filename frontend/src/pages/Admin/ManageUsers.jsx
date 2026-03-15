983478f7058208ea3d8a9ae8e706b2be15c795d7
Exploring-glitch <deysasha262@gmail.com>
2026-03-15 20:50:15 +0530

    feat: add Team Members header and download report button

--- frontend/src/pages/Admin/ManageUsers.jsx
+++ frontend/src/pages/Admin/ManageUsers.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout.jsx'
 import { get_all_users } from '../../api/userApi.js';
 import { useEffect } from 'react';
+import { LuFileSpreadsheet } from 'react-icons/lu';
 
 const ManageUsers = () => {
   const [allUsers, setAllUsers] = useState([]);
@@ -19,6 +20,11 @@ const ManageUsers = () => {
     }
   }
 
+  //download task report
+  const handleDownloadReport = async() => {
+
+  }
+
   useEffect(() => {
     getAllUsers();
 
@@ -29,7 +35,25 @@ const ManageUsers = () => {
 
   return (
     <DashboardLayout activeMenu="Team Members">
-      ManageUsers
+      <div className='mt-5 mb-10'>
+        <div className='flex justify-between
+          md:flex-row md:items-center'
+        >
+          <h2 className='text-xl font-medium'> Team Members </h2>
+
+          <button 
+            onClick={handleDownloadReport}
+            className='flex items-center gap-3 text-xs md:[text-13px] font-medium text-lime-900 bg-lime-100 px-2 md:px-3 py-2 rounded border border-lime-200 hover:border-lime-400 cursor-pointer'
+          > 
+            <LuFileSpreadsheet className='text-lg' />
+            Download report 
+          </button>
+        </div>
+
+        <div className=''>
+
+        </div>
+      </div>
     </DashboardLayout>
   )
 }
