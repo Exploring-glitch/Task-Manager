25bba8577a04f35fa631411b741adc70c7dff5cb
Exploring-glitch <deysasha262@gmail.com>
2026-03-15 20:36:29 +0530

    feat: create basic ManageUsers page

--- frontend/src/pages/Admin/ManageUsers.jsx
+++ frontend/src/pages/Admin/ManageUsers.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout.jsx'
+import { get_all_users } from '../../api/userApi.js';
+import { useEffect } from 'react';
 
 const ManageUsers = () => {
+  const [allUsers, setAllUsers] = useState([]);
+
+  const getAllUsers = async () => {
+    try {
+      const response = await get_all_users();
+
+      if (response?.length > 0) {
+        setAllUsers(response)
+      }
+    }
+    catch (err) {
+      console.log("Error fetching users. Errpr: ", err);
+    }
+  }
+
+  useEffect(() => {
+    getAllUsers();
+
+    return () => { };
+  }, []);
+
+
+
   return (
     <DashboardLayout activeMenu="Team Members">
       ManageUsers
