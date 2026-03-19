215709ab356eec841a9bb1b62d9137c9cc713a92
Exploring-glitch <deysasha262@gmail.com>
2026-03-19 22:30:18 +0530

    fix: resolve invalid excel file typo

--- frontend/src/pages/Admin/ManageUsers.jsx
+++ frontend/src/pages/Admin/ManageUsers.jsx
@@ -27,9 +27,8 @@ const ManageUsers = () => {
   const handleDownloadReport = async() => {
     try{
       const response = await download_report()
-      console.log("hello",response)
 
-      const url = window.webkitURL.createObjectURL(new Blob([response.data]))
+      const url = window.webkitURL.createObjectURL(new Blob([response]))
       const link = document.createElement("a");
       link.href = url;
       link.setAttribute("download", "user-details.xlsx")
